6681f742b9b7eb211340849fa691c35a3caf70b3
Olga Lukashenko <lukashenko.93@bk.ru>
2021-12-25 10:04:14 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,64 @@
+package ru.netology.domain;
+
+public class Radio {
+    public int currentRadioStation;
+    public int currentVolume;
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 9) {
+            return;
+        }
+        if (currentRadioStation < 0) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public int clickNextStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation = currentRadioStation + 1;
+        }
+        if (currentRadioStation == 9) {
+            currentRadioStation = 0;
+        }
+        return currentRadioStation;
+    }
+
+    public int clickPrevStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation = currentRadioStation - 1;
+        }
+        if (currentRadioStation == 0) {
+            currentRadioStation = 9;
+        }
+        return currentRadioStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public int clickPlusVolume() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+        if (currentRadioStation == 10) {
+            currentVolume = currentVolume;
+        }
+        return currentVolume;
+    }
+
+    public int clickMinusVolume() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+        if (currentRadioStation == 0) {
+            currentVolume = currentVolume;
+        }
+        return currentVolume;
+    }
+}
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,36 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void setCurrentRadioStation() {
+    }
+
+    @Test
+    void getCurrentRadioStation() {
+    }
+
+    @Test
+    void clickNextStation() {
+    }
+
+    @Test
+    void clickPrevStation() {
+    }
+
+    @Test
+    void getCurrentVolume() {
+    }
+
+    @Test
+    void clickPlusVolume() {
+    }
+
+    @Test
+    void clickMinusVolume() {
+    }
+}
